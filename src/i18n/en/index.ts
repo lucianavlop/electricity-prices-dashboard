19cbf6cc9f58041a26f3327d866cb2aabf700fd7
Daithi Hearn <daithi.hearn@gmail.com>
2023-09-06 21:42:24 +0200

    feat: auto detect locale

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,17 +1,17 @@
-import type { BaseTranslation } from "../i18n-types"
+import type { Translation } from "../i18n-types"
 
 const en = {
     TITLE: "Electricity prices",
-    TODAY_RATING: "Today {currentDate:string} is a {rating:string} day",
-    CURRENT_PRICE: "Current price - {currentTime:string}",
-    MIN_PRICE: "Min price - {minPrice:string}",
-    MAX_PRICE: "Max price - {maxPrice:string}",
+    TODAY_RATING: "Today {currentDate} is a {rating} day",
+    CURRENT_PRICE: "Current price - {currentTime}",
+    MIN_PRICE: "Min price - {minPrice}",
+    MAX_PRICE: "Max price - {maxPrice}",
     THIRTY_DAY_AVG: "30 day average",
-    TOMORROW_RATING: "Tomorrow {currentDate:string} is a {rating:string} day",
+    TOMORROW_RATING: "Tomorrow {currentDate} is a {rating} day",
     TOMORROW_NO_DATA:
         "Tomorrow's prices are not available yet. Prices are usually available after 20:30",
     LAST_THIRTY_DAYS: "Last 30 days",
     PRICE: "Price",
-} satisfies BaseTranslation
+} satisfies Translation
 
 export default en
