988a56a19e0623cf47a28b748f4da8bd08239ab2
Daithi Hearn <daithi.hearn@gmail.com>
2023-09-23 14:30:25 +0200

    feat: adding mixpanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { ThemeProvider } from "@mui/material/styles"
 import { lightTheme, darkTheme } from "Themes"
 import Dashboard from "pages/Dashboard"
@@ -6,10 +6,17 @@ import { Box, CssBaseline, createTheme, useMediaQuery } from "@mui/material"
 import TypesafeI18n from "i18n/i18n-react"
 import { loadLocale } from "i18n/i18n-util.sync"
 import { Locales } from "i18n/i18n-types"
+import mixpanel from "mixpanel-browser"
+
+mixpanel.init("c22c74cac287fb74387b7ce250f1548f")
 
 function App() {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
     const locale: Locales = navigator?.language.startsWith("en") ? "en" : "es"
+
+    useEffect(() => {
+        mixpanel.track("Website Accessed")
+    }, [])
 
     const theme = React.useMemo(
         () =>
