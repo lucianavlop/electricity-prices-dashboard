bc4616d27b3ad8ef9213c069883f4fd47a1c6ec5
Daithi Hearn <daithi.hearn@gmail.com>
2023-10-10 21:46:37 +0200

    refactor: created a hook for regional date times

--- src/utils/PriceUtils.spec.ts
+++ src/utils/PriceUtils.spec.ts
@@ -1,7 +1,5 @@
 import { filterAndPadPrices } from "./PriceUtils"
-import prices20230512 from "test/data/prices20230512.json"
 import prices20230513 from "test/data/prices20230513.json"
-import prices20230723 from "test/data/prices20230723.json"
 
 describe("filterAndPadPrices", () => {
     test("should return empty array if prices is empty", () => {
