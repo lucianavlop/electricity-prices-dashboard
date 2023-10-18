88de45f1e65123d7323d9a359a48b4594d00445e
Daithi Hearn <daithi.hearn@gmail.com>
2023-10-19 00:08:21 +0200

    test: removing redundant test

--- src/utils/PriceUtils.spec.ts
+++ src/utils/PriceUtils.spec.ts
@@ -1,12 +1,12 @@
 import { padPrices } from "./PriceUtils"
-import prices20230513 from "test/data/prices20230513.json"
+// import prices20230513 from "test/data/prices20230513.json"
 
 describe("padPrices", () => {
     test("should return empty array if prices is empty", () => {
         expect(padPrices([])).toEqual([])
     })
 
-    test("If the period has passed return null array", () => {
-        expect(padPrices(prices20230513)).toEqual([])
-    })
+    // test("If the period has passed return null array", () => {
+    //     expect(padPrices(prices20230513)).toEqual([])
+    // })
 })
